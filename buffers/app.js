22a6d4ceb9a77f9ce8b7a21e887d0f982a358523
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-14 02:08:56 +0100

    a better approach to buffer writing

--- buffers/app.js
+++ buffers/app.js
@@ -1,9 +1,16 @@
 const { Buffer } = require("buffer");
 
-const memoryContainer = Buffer.alloc(3); // allocating a 6 byte memory size
+const memoryContainer = Buffer.from([0x48, 0x69, 0x21]); // allocating a 6 byte memory size
 
-memoryContainer[0] = 0x48;
-memoryContainer[1] = 0x69;
-memoryContainer[2] = 0x21;
+// memoryContainer[0] = 0x48;
+// memoryContainer[1] = 0x69;
+// memoryContainer[2] = 0x21;
 
-console.log(memoryContainer.toString("utf-8"));
+console.log(memoryContainer.toString("utf-8")); //
+
+/**
+ *     NOTE TO SELF -
+ *    Always try to feed the data in hexadecimal form
+ * -- instead of 0100 0100 0110 1001 0010 0001 - use 0x48 0x69 0x21 --
+ *
+ */
