a1e38a6a93c55d8291a2502d2999f4bd857fe62e
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-14 01:15:55 +0100

    displaying a hi via buffers

--- buffers/app.js
+++ buffers/app.js
@@ -1,9 +1,9 @@
 const { Buffer } = require("buffer");
 
-const memoryContainer = Buffer.alloc(4); // specifying a value in bytes (4 bytes || 32 bits)
-memoryContainer[2] = 0xc4;
-memoryContainer[1] = 0xf4;
-memoryContainer[0] = 0xf1;
-memoryContainer[3] = 0x00;
+const memoryContainer = Buffer.alloc(3); // allocating a 6 byte memory size
 
-console.log(memoryContainer); // <Buffer 00 00 00 00>
+memoryContainer[0] = 0x48;
+memoryContainer[1] = 0x69;
+memoryContainer[2] = 0x21;
+
+console.log(memoryContainer.toString("utf-8"));
