aea9b8950045f51733cfc0ed0e699cdd7e8ee71d
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-16 17:25:41 +0100

    refactoring

--- fs/index.ts
+++ fs/index.ts
@@ -2,6 +2,7 @@
 
 import fs from "fs/promises";
 import path from "path";
+import event from "events";
 
 const commandFilePath = path.resolve(__dirname, "./command.txt");
 
@@ -10,20 +11,20 @@ const commandFilePath = path.resolve(__dirname, "./command.txt");
   const commandFileHandler = await fs.open(commandFilePath);
 
   for await (const event of watcher) {
-    if (event.eventType === "change") {
-      const fileDetails = await commandFileHandler.stat();
-      const buff = Buffer.alloc(fileDetails.size);
-      const offset = 0; // the location of the buffer from which we want to start filling
-      const length = buff.byteLength; // the number of bytes from the buffer to write
-      const position = 0; // from what position we want to start reading the file from
-
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position
-      );
-      console.log(content);
-    }
+    if (event.eventType === "change") readFile(commandFileHandler);
   }
 })();
+
+async function readFile(commandFileHandler: fs.FileHandle) {
+  const fileDetails = await commandFileHandler.stat(); // getting the details of the opened file
+  const buff = Buffer.alloc(fileDetails.size); // filling the buffer from the size of the opened file
+  const offset = 0; // the location of the buffer from which we want to start filling
+  const length = buff.byteLength; // the number of bytes from the buffer to write
+  const position = 0; // from what position we want to start reading the file from
+
+  // we always want to read the whole content from the beginning to the end.
+  const content = await commandFileHandler.read(buff, offset, length, position);
+
+  console.log(content);
+  return content;
+}
