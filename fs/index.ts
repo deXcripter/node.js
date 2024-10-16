d7c488d215610fcd7cb8cfee92a3ac237a416983
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-16 17:07:51 +0100

    reading the file

--- fs/index.ts
+++ fs/index.ts
@@ -1,22 +1,29 @@
+// https://nodejs.org/api/fs.html
+
 import fs from "fs/promises";
+import path from "path";
 
-const watchFile = async (path: string) => {
-  // const commandFileHandler = await fs.open(path);
-  const watch = fs.watch(path);
+const commandFilePath = path.resolve(__dirname, "./command.txt");
 
-  for await (const event of watch) {
+(async () => {
+  const watcher = fs.watch(commandFilePath);
+  const commandFileHandler = await fs.open(commandFilePath);
+
+  for await (const event of watcher) {
     if (event.eventType === "change") {
+      const fileDetails = await commandFileHandler.stat();
+      const buff = Buffer.alloc(fileDetails.size);
+      const offset = 0; // the location of the buffer from which we want to start filling
+      const length = buff.byteLength; // the number of bytes from the buffer to write
+      const position = 0; // from what position we want to start reading the file from
+
+      const content = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position
+      );
+      console.log(content);
     }
   }
-};
-const readFile = async (path?: string) => {
-  const file = await fs.readFile("./command.txt");
-};
-const main = async (path: string) => {
-  watchFile("./command.txt");
-  readFile();
-};
-
-main("./data.txt");
-
-const checkExistingFile = async (path: string) => {};
+})();
