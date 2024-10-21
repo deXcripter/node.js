3dbbece387a4444b51642f4f5d726a00ec76c8e0
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-21 15:54:49 +0100

    the create command

--- fs/main.ts
+++ fs/main.ts
@@ -9,12 +9,31 @@ import { resolve } from "path";
 // should be able to append to a file
 
 let existingFile = false;
+let dir = resolve(__dirname, "./folder");
 
 const readCommand = async () => {
   const commandFileHandler = await fs.open(resolve(__dirname, "./command.txt"));
+  const size = (await commandFileHandler.stat()).size;
+  const buffer = Buffer.alloc(size);
+  await commandFileHandler.read(buffer, 0, size, 0);
+
+  if (buffer.includes("create")) createFile("filename", "file-path");
+
+  // closing the file to avoid memory leaks
   commandFileHandler.close();
 };
-const createFile = async () => {};
+const createFile = async (name: string, path: string) => {
+  return new Promise(async (resolve, reject) => {
+    const file = await fs.open(path);
+    if (file) {
+      file.close();
+      reject("File already exists");
+    }
+
+    file.close();
+    resolve(file);
+  });
+};
 const renameFile = async () => {};
 const deleteFile = async () => {};
 const appendToFile = async () => {};
