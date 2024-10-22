dd77bab51e60df73db4a587be438dbfa063552bf
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-22 03:46:47 +0100

    optimizing

--- streams/readable-stream/read-file.ts
+++ streams/readable-stream/read-file.ts
@@ -11,7 +11,11 @@ import fs from "node:fs/promises";
     // the pressure on the memory is going to be very high considering the fact that the read speed is
     // 64kbps and the write speed is 16kbps - this would cause nodejs to buffer most of the data which
     // ends up occupying the memory space. not ideal!!!
-    streamWrite.write(chunk);
+    if (!streamWrite.write(chunk)) streamRead.pause();
+  });
+
+  streamWrite.on("drain", () => {
+    streamRead.resume();
   });
 
   streamWrite.on("drain", () => {});
