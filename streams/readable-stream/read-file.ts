f50aa1e28d5ccd07bedf1a48cb384a2e359189ec
johnpaul <dexcripter@gmail.com>
2024-11-20 20:53:58 +0100

    removing the txt large files in streams

--- streams/readable-stream/read-file.ts
+++ streams/readable-stream/read-file.ts
@@ -10,7 +10,7 @@ import fs from "node:fs/promises";
   streamRead.on("data", (chunk) => {
     // the pressure on the memory is going to be very high considering the fact that the read speed is
     // 64kbps and the write speed is 16kbps - this would cause nodejs to buffer most of the data which
-    // ends up occupying the memory space. not ideal!!!
+    // ends up occupying the memory space. not cool!!!
     if (!streamWrite.write(chunk)) streamRead.pause(); // this pauses reading the data and emits a drain event
   });
 
